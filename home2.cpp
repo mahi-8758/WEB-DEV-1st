568caee33ea998a37beb098c10afef5b507a36b4
Mahi kr. <kumarmahi8758@gmail.com>
2025-09-18 02:38:51 +0530

    CPP
    C++

---
+++ home2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void india() {
+    cout << "India is my country" << endl;
+    return;
+}
+
+int main() {
+    india();
+    int i;
+    for (i = 1; i <= 10; i++) {
+        cout << "Hello, World!" << endl;
+    }
+    return 0;
+}
